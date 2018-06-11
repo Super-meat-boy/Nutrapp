1c6a39efb58d2f1bbf439437c9cae08cee5407f8
fabien terrasse <fabien.terrasse.pf1@gmail.com>
2018-06-11 15:38:22 +0200

    Correction nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BnpSuperNavbarComponent } from './bnp-super-navbar/bnp-super-navbar.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BnpSuperNavbarComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
