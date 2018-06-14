186cf6984b97d02fb79cbf080eeeda65f8dde73c
BNPPF012\Utilisateur <jeanmarc.guicher.pfi@gmail.com>
2018-06-14 15:08:34 +0200

    rien de plus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,7 @@ import { HistoryMealsComponent } from './history-meals/history-meals.component';
     AppComponent,
     NavBarComponent,
     ContactUsComponent,
-<<<<<<< HEAD
+
 
     FooterComponent,
 
@@ -41,9 +41,9 @@ import { HistoryMealsComponent } from './history-meals/history-meals.component';
 
     HistoryMealsComponent
 
-=======
+
     FooterComponent
->>>>>>> ae4e1bd3b7746d6fe80829db65dac6b4abc4acae
+
   ],
   imports: [
     BrowserModule,
