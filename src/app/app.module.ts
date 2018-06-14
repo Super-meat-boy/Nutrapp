d70483e6d6e8c98340a509b543d504fb73f15b70
fabien terrasse <fabien.terrasse.pf1@gmail.com>
2018-06-14 14:37:22 +0200

    routes created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {
   MatInputModule,
   MatListModule,
   MatSidenavModule,
-  MatToolbarModule
+  MatToolbarModule,
 } from '@angular/material';
 
 import {NavBarComponent} from './nav-bar/nav-bar.component';
@@ -21,6 +21,18 @@ import {ContactUsComponent} from './contact-us/contact-us.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FooterComponent} from './footer/footer.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {RouterModule, Routes} from '@angular/router';
+import {MyProfileComponent} from './my-profile/my-profile.component';
+import {FavoriteMealsComponent} from './favorite-meals/favorite-meals.component';
+
+const routes: Routes = [
+  {path: 'contact', component: ContactUsComponent},
+  {path: 'myprofile', component: MyProfileComponent},
+  {path: 'favorite-meals', component: FavoriteMealsComponent},
+  {path: 'glycemic-load', component: GlycemicLoadComponent},
+  {path: 'history-meals', component: HistoryMealsComponent},
+  {path: 'list-aliment', component: ListAlilmentComponent},
+];
 
 
 @NgModule({
@@ -28,7 +40,9 @@ import {FlexLayoutModule} from '@angular/flex-layout';
     AppComponent,
     NavBarComponent,
     ContactUsComponent,
-    FooterComponent
+    FooterComponent,
+    MyProfileComponent,
+    FavoriteMealsComponent,
   ],
   imports: [
     BrowserModule,
@@ -46,6 +60,7 @@ import {FlexLayoutModule} from '@angular/flex-layout';
     MatCheckboxModule,
     FormsModule,
     MatSidenavModule,
+    RouterModule.forRoot(routes),
 
 
   ],
