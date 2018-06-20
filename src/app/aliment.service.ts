01dadaaed43cef429600a7afabccfab9646018b5
fabien terrasse <fabien.terrasse.pf1@gmail.com>
2018-06-20 17:38:52 +0200

    Tab test corrected and Materialise button on glycemic load calcul

--- src/app/aliment.service.ts
+++ src/app/aliment.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Aliment } from './aliment';
+import {Injectable} from '@angular/core';
+import {Aliment} from './aliment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,39 +7,46 @@ import { Aliment } from './aliment';
 export class AlimentService {
 
   selectedAliment: Aliment;
-  constructor() {}
-
 listeAliment: Aliment[] = [
  {
    'id': 0,
    'name': 'patate',
    'glycemie': 12,
+   'charge': 0,
  },
  {
    'id': 1,
    'name': 'celeri',
    'glycemie': 13,
+   'charge': 0,
  },
  {
    'id': 2,
    'name': 'pomme',
    'glycemie': 24,
+   'charge': 0,
  },
  {
  'id': 3,
  'name': 'courge',
  'glycemie': 15,
+   'charge': 0,
 },
 {
 'id': 0,
 'name': 'ananas',
 'glycemie': 3,
+  'charge': 0,
 },
 {
 'id': 5,
 'name': 'banane',
 'glycemie': 34,
+  'charge': 0,
 }
 ];
+
+  constructor() {
+  }
 
 }
