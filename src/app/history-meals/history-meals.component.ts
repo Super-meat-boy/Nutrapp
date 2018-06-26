ad7371428608a4665cc1d49c269c1b791f0a602b
fabien terrasse <fabien.terrasse.pf1@gmail.com>
2018-06-26 17:07:03 +0200

    translation of the app

--- src/app/history-meals/history-meals.component.ts
+++ src/app/history-meals/history-meals.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-history-meals',
@@ -7,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryMealsComponent{
 
-    displayedColumns = ['date', 'repas', 'menu', 'charge'];
+  displayedColumns = ['date', 'meal', 'menu', 'charge'];
     dataSource = ELEMENT_DATA;
   }
 
   export interface PeriodicElement {
 
     date: string;
-    repas: string;
+    meal: string;
     menu: string;
     charge: number;
 
@@ -22,16 +22,26 @@ export class HistoryMealsComponent{
 
 
   const ELEMENT_DATA: PeriodicElement[] = [
-    {date: '01 juillet 2018', repas: 'matin', menu:'Un yaourt nature', charge: 12},
-    {date: '01 juillet 2018', repas: 'dejeuner', menu:'Melon, Une omelette avec 3 oeufs, Une biscotte, Un yaourt nature', charge: 4},
-    {date: '01 juillet 2018', repas: 'diner', menu:'soupe de potiron, Une biscotte, Un yaourt nature', charge: 34},
-    {date: '02 juillet 2018', repas: 'matin', menu:'Un yaourt nature', charge: 12},
-    {date: '02 juillet 2018', repas: 'dejeuner',menu: 'Une tranche de jambon blanc, Une cuisse de poulet, Une salade de laitue, Une banane', charge: 45},
-    {date: '02 juillet 2018', repas: 'encas', menu:'Une pomme', charge: 2},
-    {date: '02 juillet 2018', repas: 'diner', menu:'soupe de poisson, Une biscotte, 300G de gruyere', charge: 28},
-    {date: '03 juillet 2018', repas: 'matin', menu:'Un yaourt nature', charge: 12},
-    {date: '03 juillet 2018', repas: 'dejeuner', menu: 'Une salade avec 2 tomates, 300gr de lentilles, Une viande rouge, Un yaourt nature', charge: 63},
-    {date: '03 juillet 2018', repas: 'encas', menu:'Une banane', charge: 4},
-    {date: '03 juillet 2018', repas: 'diner', menu:'salade avec 3 tomate, 1 oeuf, 300G de gruyere', charge: 21},
+    {date: '01 juillet 2018', meal: 'matin', menu: 'Un yaourt nature', charge: 12},
+    {date: '01 juillet 2018', meal: 'dejeuner', menu: 'Melon, Une omelette avec 3 oeufs, Une biscotte, Un yaourt nature', charge: 4},
+    {date: '01 juillet 2018', meal: 'diner', menu: 'soupe de potiron, Une biscotte, Un yaourt nature', charge: 34},
+    {date: '02 juillet 2018', meal: 'matin', menu: 'Un yaourt nature', charge: 12},
+    {
+      date: '02 juillet 2018',
+      meal: 'dejeuner',
+      menu: 'Une tranche de jambon blanc, Une cuisse de poulet, Une salade de laitue, Une banane',
+      charge: 45
+    },
+    {date: '02 juillet 2018', meal: 'encas', menu: 'Une pomme', charge: 2},
+    {date: '02 juillet 2018', meal: 'diner', menu: 'soupe de poisson, Une biscotte, 300G de gruyere', charge: 28},
+    {date: '03 juillet 2018', meal: 'matin', menu: 'Un yaourt nature', charge: 12},
+    {
+      date: '03 juillet 2018',
+      meal: 'dejeuner',
+      menu: 'Une salade avec 2 tomates, 300gr de lentilles, Une viande rouge, Un yaourt nature',
+      charge: 63
+    },
+    {date: '03 juillet 2018', meal: 'encas', menu: 'Une banane', charge: 4},
+    {date: '03 juillet 2018', meal: 'diner', menu: 'salade avec 3 tomate, 1 oeuf, 300G de gruyere', charge: 21},
 
   ]
