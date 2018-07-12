d0d5f46c8315174fd8658b0e0ed31a3c64ddd5aa
fabien terrasse <fabien.terrasse.pf1@gmail.com>
2018-07-12 16:56:55 +0200

    Calcul de la charge avec compléttion

--- src/app/glycemic-load/glycemic-load.component.ts
+++ src/app/glycemic-load/glycemic-load.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Aliment} from '../aliment';
+import {Observable} from 'rxjs';
+import {AlimentService} from '../aliment.service';
+import {map, startWith, tap} from 'rxjs/operators';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-glycemic-load',
@@ -11,10 +15,14 @@ export class GlycemicLoadComponent implements OnInit {
   //displayedColumns = ['id', 'name', 'glycemie'];
   dataSource = [];
   totalCharge = 0;
+  selectedAliment: Aliment | string = {id: null, name: '', grammage: 0, chargeGly: 0};
+  filteredOptions: Observable<Aliment[]>;
+  myControl = new FormControl();
+  options: Aliment[] = [];
 
-  alimTest: Aliment = {id: null, name: '', grammage: 0, chargeGly: 0};
+  //alimTest: Aliment = {id: null, name: '', grammage: 0, chargeGly: 0};
 
-  constructor() {
+  constructor(private alimentService: AlimentService) {
   }
 
   calculTotalCharge() {
@@ -32,12 +40,42 @@ export class GlycemicLoadComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.refresh();
   }
 
+  refresh() {
+    this.alimentService.getAll().subscribe((list) => {
+      console.log(list);
+      this.options = list;
+    });
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith<string | Aliment>(''),
+        tap(value => {
+          if (typeof value !== 'string') {
+            this.selectedAliment = value;
+          }
+          //this.selectedAliment = value;
+          console.log('this selected aliment', this.selectedAliment);
+        }),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter(name) : this.options.slice()),
+      );
+  }
   add(alim, ig, grammage) {
     console.log('test', alim);
     this.dataSource.push({id: 7, name: alim, glycemie: ig, grammage: grammage, chargeGly: (grammage * ig) / 100});
     this.calculTotalCharge();
+  }
+
+  displayFn(aliment ?: Aliment): string | undefined {
+    return aliment ? aliment.name : undefined;
+  }
+
+  private _filter(name: string): Aliment[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().indexOf(filterValue) === 0);
   }
 
 }
